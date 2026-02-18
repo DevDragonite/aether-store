3a42f185f40f468478de95263106a87cf134e64f
DevDragonite <helycamargo115@gmail.com>
2026-02-18 15:42:05 -0400

    Refactor: Removed TrustSection component per user request

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Hero } from '../components/home/Hero';
 import { FeaturedPCs } from '../components/home/FeaturedPCs';
-import { TrustSection } from '../components/home/TrustSection';
 import { NewsSection } from '../components/home/NewsSection';
 import { FAQSection } from '../components/home/FAQSection';
 
@@ -8,7 +7,6 @@ export function Home() {
     return (
         <main>
             <Hero />
-            <TrustSection />
             <FeaturedPCs />
             <NewsSection />
             <FAQSection />
